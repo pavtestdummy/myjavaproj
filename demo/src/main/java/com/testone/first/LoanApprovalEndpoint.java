23f8acc3e2689a89208d53ffe771b53fb9cb1046
pavtestdummy <pavtestdummy@gmail.com>
2024-02-09 15:47:05 +0530

    adding another endipoint for dummy external json

--- demo/src/main/java/com/testone/first/LoanApprovalEndpoint.java
+++ demo/src/main/java/com/testone/first/LoanApprovalEndpoint.java
@@ -1,24 +1,44 @@
 package com.testone.first;
 
+import org.springframework.boot.autoconfigure.elasticsearch.ElasticsearchProperties.Restclient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 
 @RestController
 public class LoanApprovalEndpoint {
-	
+
 	@GetMapping("/loancheck/{years}")
 	public ResponseEntity<LoanResponse> getLoanStatus(@PathVariable("years")int years)
 	{
 	     LoanResponse rs=null;
-		System.out.println("in loancheck get loan stagus "+years);
+		System.out.println("in loancheck get loan stagus 3.2.2 "+years);
 		if(years < 5)
 	        rs=new LoanResponse(0, years, 0);
 		else
 			rs=new LoanResponse(years*2000, years, 1);
 		
 		return ResponseEntity.ok(rs);
+	}
+
+
+	@GetMapping("/postmsg/{id}")
+	public ResponseEntity<TestPost> getPostMsg(@PathVariable("id")int id)
+	{
+	     
+		TestPost tp=null;
+		String uriBase="https://jsonplaceholder.typicode.com/posts/";
+		RestClient restClient=RestClient.create();
+		System.out.println(" in post test "+id);
+		tp=restClient.get()		
+				  .uri(uriBase + id)
+				  .retrieve()
+				  .body(TestPost.class);
+		
+		return ResponseEntity.ok(tp);
+		
 	}
 
 }
---
+++ demo/src/main/java/com/testone/first/TestPost.java
@@ -0,0 +1,42 @@
+package com.testone.first;
+
+public class TestPost {
+ String userId;
+ String id;
+ String title;
+ String body;
+public String getUserId() {
+	return userId;
+}
+public void setUserId(String userId) {
+	this.userId = userId;
+}
+public String getId() {
+	return id;
+}
+public void setId(String id) {
+	this.id = id;
+}
+public String getTitle() {
+	return title;
+}
+public void setTitle(String title) {
+	this.title = title;
+}
+public String getBody() {
+	return body;
+}
+public void setBody(String body) {
+	this.body = body;
+}
+public TestPost(String userId, String id, String title, String body) {
+	super();
+	this.userId = userId;
+	this.id = id;
+	this.title = title;
+	this.body = body;
+}
+ public TestPost() {
+	// TODO Auto-generated constructor stub
+}
+}
